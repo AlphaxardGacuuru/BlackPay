689e597960a975df19b69e179edc3eecfa62823a
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-07-12 10:30:01 +0300

    Added dependancies

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
+use App\User;
 use Laravel\Socialite\Facades\Socialite;
-
+use Illuminate\Support\Facades\Auth;
 
 
 class LoginController extends Controller
